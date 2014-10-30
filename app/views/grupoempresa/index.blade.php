ee1fcaf3bef078301991896dc7ba707774d427b9
vladwelt <vladwelt@gmail.com>
2014-10-30 09:01:56 -0400

    [ADD] Vista de grupo empresa funciionando

--- app/views/grupoempresa/index.blade.php
+++ app/views/grupoempresa/index.blade.php
@@ -1,14 +1,38 @@
-
-
 @extends('plantillas.principal')
 @section('cabecera')
 
 {{ HTML::style('css/login.css') }}
+{{ HTML::style('css/grupo-empresa.css') }}
 
 @stop
 
 @section('contenido')
-	<a href="grupoempresa/subirdocumento">Subir parte A</a>
-	<a href="grupoempresa/registrarsocio">Registrar Socio</a>
 
+<div id="wrapper">
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav nav nav-pills nav-stacked" role="tablist">
+            <li class=""><a class="list-group-item" href="grupoempresa/subirdocumento">Proyecto</a></li>
+            <li><a class="list-group-item" href="grupoempresa/registrarsocio">Socio</a></li>
+            <li><a class="list-group-item" href="#">Calendario</a></li>
+        </ul>
+    </div>
+
+    <div id="page-content-wrapper">
+        <div class="page-content">
+            <div class="container">
+                <h2>Actividades</h2>
+                <ul>
+                    <li><p>25 Sep: Consultor Juan Perez solicito una cita con usted.</p></li>
+
+                    <li><p>25 Sep: Consultor Juan Perez solicito una cita con usted.</p></li>
+                </ul>
+                <div class="row">
+                    <div class="col-md-12">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
