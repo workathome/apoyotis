40478eadd0f992fb40abef9b6520fa329b682c99
vladwelt <vladwelt@gmail.com>
2014-11-05 23:13:06 -0400

    [EDIT] Renombrando variables.

--- app/views/consultor/index.blade.php
+++ app/views/consultor/index.blade.php
@@ -5,12 +5,10 @@
 
 @section('contenido')
 <div class="container">
-        <h1>vista consultor</h1>
-
 <div id="wrapper">
     <div id="sidebar-wrapper">
         <ul class="sidebar-nav nav nav-pills nav-stacked" role="tablist">
-            <li class=""><a class="list-group-item" href="consultor/subirdocpublico">Subir Documento Publico</a></li>
+            <li class=""><a class="list-group-item" href="consultor/subirdocpublico">Subir Documento</a></li>
             <li><a class="list-group-item" href="">Proyecto</a></li>
             <li><a class="list-group-item" href="#">Grupo Empresas</a></li>
         </ul>
@@ -22,7 +20,7 @@
                 <h2>Actividades</h2>
                 <ul>
                     
-                    @foreach($documentos as $documento)
+                    @foreach($documentos_empresas as $documento)
                         <li><p><strong>{{$documento->updated_at}}</strong>,  La Grupo-Empresa <strong>{{$documento->grupoempresa["nombrelargoge"]}}</strong>Subio un documento <strong>{{$documento->titulo_gedocumento }}</strong> con la siguiente descripción {{$documento->descripciongedocumento}} <a href={{$documento->pathdocumentoge }}>    <span class="glyphicon glyphicon-save"></span></a></p></li>
                     @endforeach
                 </ul>
