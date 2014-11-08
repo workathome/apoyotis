2af58d803780ed5dceadb7aef3635b7911672b78
vladwelt <vladwelt@gmail.com>
2014-11-08 18:38:51 -0400

    [ADD] Validadores en registro socio de grupo empresa.

--- app/views/grupoempresa/registrarsocio.blade.php
+++ app/views/grupoempresa/registrarsocio.blade.php
@@ -2,6 +2,9 @@
 @section('cabecera')
 
 {{ HTML::style('css/login.css') }}
+{{ HTML::style('css/bootstrapValidator.css') }}
+{{ HTML::script('js/bootstrapValidator.js') }}
+{{ HTML::script('js/validatorSocio.js') }}
 
 @stop
 
@@ -10,74 +13,86 @@
         @if (Session::has('mensaje'))
             <div class="alert alert-warning" role="alert">{{ Session::get('mensaje') }}</div>
         @endif
-    {{ Form::open(array('files'=>true, 'class'=>'form-signin') ) }}
+    {{ Form::open(array('files'=>true, 'class'=>'form-signin', 'id'=>'validatorForm') ) }}
         <h1>Registrar Socio :</h1>
-		{{ Form::label('nombresocio', 'Nombre Socio'); }}
-        @if( $errors->has('nombresocio') )
-            @foreach($errors->get('nombresocio') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::text('nombresocio',Input::old('nombresocio'), array('class'=>'form-control')); }}
-
-        {{ Form::label('apellidossocio', 'Apellidos Socio'); }}
-        @if( $errors->has('apellidossocio') )
-            @foreach($errors->get('apellidossocio') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::text('apellidossocio',Input::old('apellidossocio'), array('class'=>'form-control')); }}
-
-
-        {{ Form::label('estadocivil', 'Estado Civil'); }}
-        @if( $errors->has('estadocivil') )
-            @foreach($errors->get('estadocivil') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::select('estadocivil', $estodos_civil , array('class' => 'form-control')); }}
-
-        {{ Form::label('direccion', 'Dirección'); }}
-        @if( $errors->has('direccion') )
-            @foreach($errors->get('direccion') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::text('direccion',Input::old('direccion'), array('class'=>'form-control')); }}
-
-        {{ Form::label('cargo', 'Cargo'); }}
-        @if( $errors->has('cargo') )
-            @foreach($errors->get('cargo') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::text('cargo',Input::old('cargo'), array('class'=>'form-control')); }}
-
-        {{ Form::label('correoelectronicosocio', 'Correo electronico'); }}
-        @if( $errors->has('correoelectronicosocio') )
-            @foreach($errors->get('correoelectronicosocio') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::text('correoelectronicosocio',Input::old('correoelectronicosocio'), array('class'=>'form-control')); }}
-
-        {{ Form::label('telefonosocio', 'Teléfono'); }}
-        @if( $errors->has('telefonosocio') )
-            @foreach($errors->get('telefonosocio') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::text('telefonosocio',Input::old('telefonosocio'), array('class'=>'form-control')); }}
-
-        {{ Form::label('tipo_socio_codtipo_socio', 'tipo socio'); }}
-        @if( $errors->has('tipo_socio_codtipo_socio') )
-            @foreach($errors->get('tipo_socio_codtipo_socio') as $error )
-                <br />* {{ $error }}
-            @endforeach
-        @endif
-        {{ Form::select('tipo_socio_codtipo_socio',  $tiposocio,array('class' => 'form-control')); }}
-
-        {{ Form::submit('Subir',array('class'=>'btn-primary btn btn-1g btn-block')); }}
+        <div class="form-group">
+	    {{ Form::label('nombresocio', 'Nombre Socio', array('class'=>'control-label')); }}
+            @if( $errors->has('nombresocio') )
+                @foreach($errors->get('nombresocio') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            {{ Form::text('nombresocio',Input::old('nombresocio'), array('class'=>'form-control', 'name'=>'nombresocio')); }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('apellidossocio', 'Apellidos Socio', array('class'=>'control-label')); }}
+            @if( $errors->has('apellidossocio') )
+                @foreach($errors->get('apellidossocio') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            {{ Form::text('apellidossocio',Input::old('apellidossocio'), array('class'=>'form-control', 'name'=>'apellidossocio')); }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('estadocivil', 'Estado Civil', array('class'=>'control-label')); }}
+            @if( $errors->has('estadocivil') )
+                @foreach($errors->get('estadocivil') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            {{ Form::select('estadocivil', $estodos_civil , array('class' => 'form-control', 'name'=>'estadocivil')); }}
+        </div>
+        <div  class="form-group">
+            {{ Form::label('direccion', 'Dirección', array('class'=>'control-label')); }}
+            @if( $errors->has('direccion') )
+                @foreach($errors->get('direccion') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            {{ Form::text('direccion',Input::old('direccion'), array('class'=>'form-control', 'name'=>'direccion')); }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('cargo', 'Cargo', array('class'=>'control-label')); }}
+            @if( $errors->has('cargo') )
+                @foreach($errors->get('cargo') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            {{ Form::text('cargo',Input::old('cargo'), array('class'=>'form-control', 'name'=>'cargo')); }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('correoelectronicosocio', 'Correo electronico', array('class'=>'control-label')); }}
+            @if( $errors->has('correoelectronicosocio') )
+                @foreach($errors->get('correoelectronicosocio') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            <div class="input-group">
+            <div class="input-group-addon">@</div>
+            {{ Form::text('correoelectronicosocio',Input::old('correoelectronicosocio'), array('class'=>'form-control', 'name'=>'correoelectronicosocio')); }}
+        </div>
+        </div>
+        <div class="form-group">
+            {{ Form::label('telefonosocio', 'Teléfono', array('class'=>'control-label')); }}
+            @if( $errors->has('telefonosocio') )
+                @foreach($errors->get('telefonosocio') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            {{ Form::text('telefonosocio',Input::old('telefonosocio'), array('class'=>'form-control', 'name'=>'telefonosocio')); }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('tipo_socio_codtipo_socio', 'Tipo socio', array('class'=>'control-label')); }}
+            @if( $errors->has('tipo_socio_codtipo_socio') )
+                @foreach($errors->get('tipo_socio_codtipo_socio') as $error )
+                    <br />* {{ $error }}
+                @endforeach
+            @endif
+            {{ Form::select('tipo_socio_codtipo_socio',  $tiposocio,array('class' => 'form-control', 'name'=>'tipo_socio_codtipo_socio')); }}
+        </div>
+        <div  class="form-group">
+            {{ Form::submit('Subir',array('class'=>'btn-primary btn btn-1g btn-block')); }}
+        </div>
     {{ Form::close() }}
 </div>
 @stop
---
+++ public/js/validatorSocio.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+    $('#validatorForm').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        live: 'enabled',
+        fields: {
+            nombresocio: {
+                validators: {
+                    notEmpty: {
+                        message: 'El nombre de socio es necesario'
+                    },
+                    stringLength: {
+                        min: 5,
+                        max: 40,
+                        message: 'El nombre del socio debe tener al menos 3 y no mas de 40 caracteres'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z]+$/,
+                        message: 'El nombre de socio consiste solo de letras.'
+                    }
+                }
+            },
+            apellidossocio: {
+                validators: {
+                    notEmpty: {
+                        message: 'El Apellido de socio es necesario'
+                    },
+                    stringLength: {
+                        min: 5,
+                        max: 30,
+                        message: 'El Apellido del socio debe tener al menos 3 y no mas de 30 caracteres'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z]+$/,
+                        message: 'El Apellido de socio consiste solo de letras.'
+                    }
+                }
+            },
+            estadocivil: {
+                validators: {
+                    notEmpty: {
+                        message: 'Seleccione su estado civil'
+                    }
+                }
+            },
+            direccion: {
+                validators: {
+                    notEmpty: {
+                        message: 'La direccion es necesaria'
+                    }
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9]+$/,
+                        message: 'La direccion consiste de caracteres alfanumericos y numeros'
+                    }
+                }
+            },
+            cargo: {
+                validators: {
+                    notEmpty: {
+                        message: 'El cargo es necesario'
+                    },
+                    regexp: {
+                        message: 'El cargo consiste solo de letras'
+                    }
+                }
+            },
+            correoelectronicosocio: {
+                validators: {
+                    notEmpty: {
+                        message: 'El correo electronico es necesario'
+                    },
+                    emailAddress: {
+                        message: 'No es un correo electronico valido!'
+                    } 
+                }
+            },
+            telefonosocio: {
+                validators: {
+                    notEmpty: {
+                        message: 'El telefono es necesario'
+                    },
+                    regexp: {
+                        regexp: /^[0-9]+$/,
+                        message: 'El telefono de socio debe contener solo numeros'
+                    },
+                    stringLenght: {
+                        min: 7,
+                        max: 8,
+                        message: 'El telefono debe contener entre 7 y 8 numeros'
+                    }
+                }
+            },
+            tipo_socio_codtipo_socio: {
+                validators: {
+                    notEmpty: {
+                        message: 'Seleccione un tipo de socio'
+                    }
+                }
+            }
+        }
+    });
+});
