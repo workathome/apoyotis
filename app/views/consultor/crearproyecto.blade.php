ad8e8b821a21a85403e466dff65a823c9d53dbb0
vladwelt <vladwelt@gmail.com>
2014-11-11 01:49:42 -0400

    [EDIT] Correcion en idioma y cerrado de calendario.

--- app/views/consultor/crearproyecto.blade.php
+++ app/views/consultor/crearproyecto.blade.php
@@ -1,10 +1,11 @@
 @extends('plantillas.consultor')
 @section('cabecera1')
-{{ HTML::script('js/moment.min.js')}}
+{{ HTML::style('css/bootstrapValidator.min.css') }}
+{{ HTML::script('js/bootstrapValidator.min.js') }}
+{{ HTML::script('js/moment-with-locales.min.js')}}
 {{ HTML::script('js/bootstrap-datetimepicker.min.js') }}
 {{ HTML::style('css/bootstrap-datetimepicker.min.css') }}
 {{ HTML::script('js/validatorProyecto.js') }}
-
 @stop
 @section('contenido1')
 
@@ -15,28 +16,33 @@
             <div class="alert {{ Session::get('mensaje')[0] }}" role="alert">{{ Session::get('mensaje')[1] }}</div>
         @endif
         <div>
-            {{ Form::open(array('class'=>'form-inline') ) }}
-                {{ Form::label('nombreproyecto','Nombre Proyecto'); }}
+            {{ Form::open(array('class'=>'form-inline', 'id'=>'projectForm') ) }}
+            <div class="form-group">
+                {{ Form::label('nombreproyecto','Nombre Proyecto',array('class'=>'control-label')); }}
                 @if( $errors->has('nombreproyecto') )
                     @foreach($errors->get('nombreproyecto') as $error )
                         <br />* {{ $error }}
                     @endforeach
                 @endif
                 {{ Form::text('nombreproyecto',Input::old('nombreproyecto') , array('class'=>'form-control')); }}
-                {{ Form::label('fechafinproyecto','Fecha fin proyecto') }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('fechafinproyecto','Fecha fin proyecto', array('class'=>'control-label')) }}
                 @if( $errors->has('fechafinproyecto') )
                     @foreach($errors->get('fechafinproyecto') as $error )
                         <br />* {{ $error }}
                     @endforeach
                 @endif
-                <div class="form-group">
+
                 <div class='input-group date' id='datetimepicker1'>
-                    {{ Form::text('fechafinproyecto', Input::old('fechafinproyecto') , array('class'=>'form-control')); }}
                     <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
                     </span>
+                    {{ Form::text('fechafinproyecto', Input::old('fechafinproyecto') , array('class'=>'form-control','name'=>'fechafinproyecto')); }}
                 </div>
-                </div>
-                {{ Form::submit('Crear',array('class'=>'btn-primary btn')); }}
+            </div>
+            <div class="form-group">
+            {{ Form::submit('Crear',array('class'=>'btn-primary btn')); }}
+            </div>
             {{ Form::close() }}
         </div>
     </div>
--- public/js/validatorProyecto.js
+++ public/js/validatorProyecto.js
@@ -1,10 +1,43 @@
-//$(document).ready(function(){
-    
-//});
+$(document).ready(function(){
+    $('#projectForm').bootstrapValidator({
+        feedbackIcons: { 
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        live: 'enabled',
+        fields: {
+            nombreproyecto: {
+                validators: {
+                    notEmpty: {
+                        message: 'El nombre del proyecto es necesario'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9\s.\-ñáéíóú\(\)]+$/,
+                        message: 'El nombre del proyecto solo contiene '
+                    }
+                }
+            },
+            fechafinproyecto: {
+                validators: {
+                    notEmpty: {
+                        message: "La fecha fin es necesaria"
+                    },
+                    regexp: {
+                        regexp: /^[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}$/,
+                        message: 'La fecha no soporta ese formato'
+                    }
+                }
+            }
+        }
+    });
+});
 
 $(function () {
     $('#datetimepicker1').datetimepicker({
         format : 'YYYY-MM-DD HH:mm:ss',
-        showSeconds: true
+        showSeconds: true,
+        language: "es",
+        autoclose:true
     });
 });
