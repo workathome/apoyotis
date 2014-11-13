75a4d0b51a09efc9e5865e6cad0105a90539b688
vladwelt <vladwelt@gmail.com>
2014-11-13 09:10:48 -0400

    [EDIT] Vista responsiva con Grids en registrar socio

--- app/views/grupoempresa/registrarsocio.blade.php
+++ app/views/grupoempresa/registrarsocio.blade.php
@@ -9,14 +9,21 @@
 @stop
 
 @section('contenido1')
-        
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="page-header">
+            Socios <small> Registrar Socio</small></div>
+            </h2>    
+        </div>
+    </div>
+    <div class="row">        
         <div class="col-sm-12">
             <ol class="breadcrumb">
                 <li><a href="/grupoempresa"><i class="fa fa-fw fa-home"></i> Inicio</a></li>
                 <li class="active"><a><i class="fa fa-fw fa-users"></i> Socios</a></li>
             </ol>
         </div>
-
+    </div>
         @if (Session::has('mensaje'))
             <div class="alert alert-warning" role="alert">{{ Session::get('mensaje') }}</div>
         @endif
@@ -46,31 +53,40 @@
         </tbody>
     </table>
         </ul>
-
-    {{ Form::open(array('files'=>true, 'class'=>'form-signin', 'id'=>'validatorForm') ) }}
-        <h1>Registrar Socio :</h1>
-        <div class="form-group">
-	    {{ Form::label('nombresocio', 'Nombre Socio', array('class'=>'control-label')); }}
-            @if( $errors->has('nombresocio') )
-                @foreach($errors->get('nombresocio') as $error )
-                    <br />
-                    <span class="label label-danger"> * {{ $error }} </span>
-                @endforeach
-            @endif
-            {{ Form::text('nombresocio',Input::old('nombresocio'), array('class'=>'form-control', 'name'=>'nombresocio')); }}
+    <div class="form">
+    {{ Form::open(array('files'=>true, 'class'=>'form-horizontal', 'id'=>'validatorForm') ) }}
+        <div class="row">
+            <div class="form-group">
+                {{ Form::label('nombresocio', 'Nombre Socio', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+                <div class="col-md-3">
+                    @if( $errors->has('nombresocio') )
+                        @foreach($errors->get('nombresocio') as $error )
+                            <br />
+                            <span class="label label-danger"> * {{ $error }} </span>
+                        @endforeach
+                    @endif
+                    {{ Form::text('nombresocio',Input::old('nombresocio'), array('class'=>'form-control', 'name'=>'nombresocio')); }}
+                </div>
+            </div>
         </div>
+    <div class="row">
         <div class="form-group">
-            {{ Form::label('apellidossocio', 'Apellidos Socio', array('class'=>'control-label')); }}
-            @if( $errors->has('apellidossocio') )
-                @foreach($errors->get('apellidossocio') as $error )
-                    <br />
-                    <span class="label label-danger"> * {{ $error }} </span>
-                @endforeach
-            @endif
-            {{ Form::text('apellidossocio',Input::old('apellidossocio'), array('class'=>'form-control', 'name'=>'apellidossocio')); }}
+            {{ Form::label('apellidossocio', 'Apellidos Socio', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+            <div class="col-md-3">
+                @if( $errors->has('apellidossocio') )
+                    @foreach($errors->get('apellidossocio') as $error )
+                        <br />
+                        <span class="label label-danger"> * {{ $error }} </span>
+                    @endforeach
+                @endif
+                {{ Form::text('apellidossocio',Input::old('apellidossocio'), array('class'=>'form-control', 'name'=>'apellidossocio')); }}
+            </div>
         </div>
+    </div>
+    <div class="row">
         <div class="form-group">
-            {{ Form::label('estadocivil', 'Estado Civil', array('class'=>'control-label')); }}
+            {{ Form::label('estadocivil', 'Estado Civil', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+            <div class="col-md-3">
             @if( $errors->has('estadocivil') )
                 @foreach($errors->get('estadocivil') as $error )
                     <br />
@@ -78,9 +94,13 @@
                 @endforeach
             @endif
             {{ Form::select('estadocivil', $estodos_civil , Input::old('estadocivil'), array('class' => 'form-control', 'name'=>'estadocivil')); }}
+            </div>
         </div>
+    </div>
+    <div class="row">
         <div  class="form-group">
-            {{ Form::label('direccion', 'Dirección', array('class'=>'control-label')); }}
+            {{ Form::label('direccion', 'Dirección', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+            <div class="col-md-3">
             @if( $errors->has('direccion') )
                 @foreach($errors->get('direccion') as $error )
                     <br />
@@ -88,9 +108,13 @@
                 @endforeach
             @endif
             {{ Form::text('direccion',Input::old('direccion'), array('class'=>'form-control', 'name'=>'direccion')); }}
+            </div>
         </div>
+    </div>
+    <div class="row">
         <div class="form-group">
-            {{ Form::label('cargo', 'Cargo', array('class'=>'control-label')); }}
+            {{ Form::label('cargo', 'Cargo', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+            <div class="col-md-3">
             @if( $errors->has('cargo') )
                 @foreach($errors->get('cargo') as $error )
                     <br />
@@ -98,32 +122,44 @@
                 @endforeach
             @endif
             {{ Form::text('cargo',Input::old('cargo'), array('class'=>'form-control', 'name'=>'cargo')); }}
+            </div>
         </div>
+    </div>
+    <div class="row">
         <div class="form-group">
-            {{ Form::label('correoelectronicosocio', 'Correo electronico', array('class'=>'control-label')); }}
-            @if( $errors->has('correoelectronicosocio') )
-                @foreach($errors->get('correoelectronicosocio') as $error )
-                    <br />
-                    <span class="label label-danger"> * {{ $error }} </span>
-                @endforeach
-            @endif
-            <div class="input-group">
-            <div class="input-group-addon">@</div>
-            {{ Form::text('correoelectronicosocio',Input::old('correoelectronicosocio'), array('class'=>'form-control', 'name'=>'correoelectronicosocio')); }}
+            {{ Form::label('correoelectronicosocio', 'Correo electronico', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+            <div class="col-md-3">
+                @if( $errors->has('correoelectronicosocio') )
+                    @foreach($errors->get('correoelectronicosocio') as $error )
+                        <br />
+                        <span class="label label-danger"> * {{ $error }} </span>
+                    @endforeach
+                @endif
+                <div class="input-group">
+                    <div class="input-group-addon">@</div>
+                    {{ Form::text('correoelectronicosocio',Input::old('correoelectronicosocio'), array('class'=>'form-control', 'name'=>'correoelectronicosocio')); }}
+                </div>
+            </div>
         </div>
-        </div>
+    </div>
+    <div class="row">
         <div class="form-group">
-            {{ Form::label('telefonosocio', 'Teléfono', array('class'=>'control-label')); }}
-            @if( $errors->has('telefonosocio') )
-                @foreach($errors->get('telefonosocio') as $error )
-                    <br />
-                    <span class="label label-danger"> * {{ $error }} </span>
-                @endforeach
-            @endif
-            {{ Form::text('telefonosocio',Input::old('telefonosocio'), array('class'=>'form-control', 'name'=>'telefonosocio')); }}
+            {{ Form::label('telefonosocio', 'Teléfono', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+            <div class="col-md-3">
+                @if( $errors->has('telefonosocio') )
+                    @foreach($errors->get('telefonosocio') as $error )
+                        <br />
+                        <span class="label label-danger"> * {{ $error }} </span>
+                    @endforeach
+                @endif
+                {{ Form::text('telefonosocio',Input::old('telefonosocio'), array('class'=>'form-control', 'name'=>'telefonosocio')); }}
+            </div>
         </div>
+    </div>
+    <div class="row">
         <div class="form-group">
-            {{ Form::label('tipo_socio_codtipo_socio', 'Tipo socio', array('class'=>'control-label')); }}
+            {{ Form::label('tipo_socio_codtipo_socio', 'Tipo socio', array('class'=>'control-label col-md-offset-3 col-md-2')); }}
+            <div class="col-md-3">
             @if( $errors->has('tipo_socio_codtipo_socio') )
                 @foreach($errors->get('tipo_socio_codtipo_socio') as $error )
                     <br />
@@ -131,9 +167,17 @@
                 @endforeach
             @endif
             {{ Form::select('tipo_socio_codtipo_socio',  $tiposocio, Input::old('tipo_socio_codtipo_socio'), array('class' => 'form-control', 'name'=>'tipo_socio_codtipo_socio')); }}
+            </div>
         </div>
+    </div>
+    <div class="row">
         <div  class="form-group">
-            {{ Form::submit('Registrar',array('class'=>'btn-primary btn btn-1g btn-block')); }}
+            <div class="col-md-offset-5 col-md-2">
+                {{ Form::submit('Registrar',array('class'=>'btn-primary btn btn-1g btn-block')); }}
+            </div>
         </div>
     {{ Form::close() }}
+    </div>
+    </div>
+    </div>
 @stop
