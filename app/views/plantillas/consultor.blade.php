52e6ace89b891cf491f8c9a573f93e459180842b
vladwelt <vladwelt@gmail.com>
2014-11-20 09:29:13 -0400

    [EDIT] cambio de vistas con cambios en el sidebar editados con laravel.

--- app/views/plantillas/consultor.blade.php
+++ app/views/plantillas/consultor.blade.php
@@ -7,16 +7,28 @@
 @section('navegacion')
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
-            <li><a href="/consultor"><i class="fa fa-fw fa-home"></i> Inicio</a></li>
+            <li class="{{Request::path() == 'consultor' ? 'active' : '';}}"><a href="/consultor"><i class="fa fa-fw fa-home"></i> Inicio</a></li>
 
-            <li class="active">
+            <li class="{{Request::path() == 'consultor/proyecto' | Request::path() == 'consultor/crearproyecto' ? 'active' : '';}}">
             @if(Proyecto::vigente())
                 <a href="/consultor/proyecto"><i class="fa fa-fw fa-cogs"></i> Proyecto</a>
             @else
                 <a href="/consultor/crearproyecto"><i class="fa fa-fw fa-plus"></i> Crear Proyecto</a>
             @endif
             <li>
-            <li><a href="/consultor/grupoempresas"><i class="fa fa-fw fa-users"></i> Grupo Empresas</a></li>
+            <li class="{{Request::path() == 'consultor/grupoempresas' ? 'active' : '';}}"><a href="javascript:;" data-toggle="collapse" data-target="#dropGrupoEmpresas"><i class="fa fa-fw fa-users"></i> Grupo Empresas</a>
+                <!--<a href="/consultor/grupoempresas"><i class="fa fa-fw fa-users"></i> Grupo Empresas</a>-->
+                @if(isset($consultor_empresas))
+                <ul id="dropGrupoEmpresas" class="collapse">
+
+                    @foreach($consultor_empresas as $empresa)
+                        <li><a href="/consultor/grupoempresas"><i class="fa fa-fw fa-chevron-right"></i>
+                        {{ $empresa->grupoempresa->nombrelargoge }}</a></li>
+                    @endforeach
+                </ul>
+                @endif
+            </li>
+
         </ul>
     </div>
 @stop
--- app/views/plantillas/grupo-empresa.blade.php
+++ app/views/plantillas/grupo-empresa.blade.php
@@ -1,14 +1,15 @@
 @extends('plantillas.principal')
 @section('cabecera')
 {{ HTML::style('css/grupo-empresa.css') }}
+{{ HTML::script('js/grupo-empresa.js') }}
 @yield('cabecera1')
 @stop
 @section('navegacion')
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
-            <li class="active"><a href="/grupoempresa">
+            <li class="{{Request::path() == 'grupoempresa' ? 'active' : '';}}"><a href="/grupoempresa">
             <i class="fa fa-fw fa-home"></i>Inicio</a></li>
-            <li><a href="javascript:;" data-toggle="collapse" data-target="#dropProyecto">
+            <li class="{{Request::path() == 'grupoempresa/subirdocumento' ? 'active' : '';}}"><a href="javascript:;" data-toggle="collapse" data-target="#dropProyecto">
                 <i class="fa fa-fw fa-cog"></i> Proyecto
                 <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="dropProyecto" class="collapse">
@@ -16,7 +17,7 @@
                         <i class="fa fa-fw fa-upload"></i> Subir parte A</a></li>
                 </ul>
             </li>
-            <li><a href="/grupoempresa/registrarsocio"><i class="fa fa-fw fa-users"></i> Socios</a></li>
+            <li class="{{Request::path() == 'grupoempresa/registrarsocio' ? 'active' : '';}}"><a href="/grupoempresa/registrarsocio"><i class="fa fa-fw fa-users"></i> Socios</a></li>
             <li><a href="#"><i class="fa fa-fw fa-calendar"></i> Calendario</a></li>
         </ul>
     </div>
