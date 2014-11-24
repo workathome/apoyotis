3c0af26d2298171932e11d2ff5618104ff6b3d70
vladwelt <vladwelt@gmail.com>
2014-11-24 18:23:02 -0400

    [EDIT][TEST] Genera un archvo pdf y este es mostrado y refrescado cada vez que hay un submit.

--- app/views/test.blade.php
+++ app/views/test.blade.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
 <html ng-app="generador">
     <head ng-non-bindable>
+        {{ HTML::script('components/jquery/dist/jquery.min.js') }}
         {{ HTML::style('components/bootstrap/dist/css/bootstrap.min.css') }}
         {{ HTML::script('components/bootstrap/dist/js/bootstrap.min.js') }}
-        {{ HTML::script('components/jquery/dist/jquery.min.js') }}
         {{ HTML::script('components/angular/angular.min.js') }}
         {{ HTML::script('js/angularPdf.js') }}
     </head>
     <body>
-        <div ng-controller="pdfController">
-            @if ($pdf)
-                    <a target="_blank" href="{{$pdf}}">Descargar Pdf Generado</a>
-            @endif
-            <h1 ng-if="fileExists()">@{{mensaje}}</h1>
-            {{ Form::open(array('files'=>true, 'class'=>'form-inline', 'id'=>'upForm') ) }}
-                <h1>Generar Latex</h1>
-                {{Form::textarea("latex", $esqueleto ,array("cols"=>"130","rows"=>"50"))}}
-                {{ Form::submit('Subir',array('class'=>'btn-primary btn btn-md btn-block')); }}
-            {{ Form::close() }}
+        <div ng-controller="pdfController" class="row">
+            <div class="col-md-6">
+                @if ($pdf)
+                        <a target="_blank" href="{{$pdf}}">Descargar Pdf Generado</a>
+                @endif
+                {{ Form::open(array('files'=>true, 'class'=>'form-inline', 'id'=>'upForm') ) }}
+                    <h1>Generar Latex</h1>
+                    <div class="form-group">
+                        {{Form::textarea("latex", $esqueleto ,array("cols"=>"60","rows"=>"50"))}}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::submit('Subir',array('class'=>'btn-primary btn btn-md btn-block')); }}
+                    </div>
+                {{ Form::close() }}
+            </div>
+            <div ng-if="fileExists()" class="col-md-6">
+                <iframe id="viewer" width='400' height='300' src = "/components/bower-viewerjs/#{{$pdf}}" allowfullscreen webkitallowfullscreen></iframe> 
+            
+            </div>
         </div>
     </body>
 </html>
