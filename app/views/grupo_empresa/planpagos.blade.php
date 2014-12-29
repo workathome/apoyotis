6390c3af0740d20659df0b3f0dacf720be7b989f
vladwelt <vladwelt@gmail.com>
2014-12-29 01:39:21 -0400

    [EDIT] css afinados para produccion.

--- app/views/grupo_empresa/planpagos.blade.php
+++ app/views/grupo_empresa/planpagos.blade.php
@@ -104,27 +104,27 @@
             </div>
         </div> 
         <div class="row">
-            <form class="form-inline" role="form" ng-submit="addHito()">
-                <div class="col-md-3">
+            <form class="form-inline" role="form" ng-submit="addHito()" style="margin:auto;">
+                <div class="col-md-2">
                     <label for="nombre">Nombre de Hito</label>
                     <input ng-model="hito.nombre" type="text" class="form-control" id="nombre" placeholder="Ingrese el nombre del Hito">
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <label for="fecha">Fecha de Entrega</label>
                     <div class="input-group date" id="datetimepicker3">
                         <input ng-model="hito.fecha" type="text" class="form-control" id="fecha" placeholder="Ingrese la fecha" onfocus="blur();">
                         <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <label for="entregables">Entregables</label>
                     <input ng-model="hito.entregables" type="text" class="form-control" id="entregables" placeholder="entregables (separe con comas)">
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <label for="porcentaje">Porcentaje(%)</label>
                     <input ng-model="hito.porcentaje" type="text" class="form-control" id="porcentaje" placeholder="Porcentaje del hito">
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <button class="btn btn-primary">Agregar</button>
                 </div>
             </form>
