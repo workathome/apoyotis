2551a7d08ab44dfc1d669fc57040f2341a0a813a
vladwelt <vladwelt@gmail.com>
2014-12-28 23:35:42 -0400

    [FIXED] close cambiado por cancelar.

--- app/views/proyecto/principal.blade.php
+++ app/views/proyecto/principal.blade.php
@@ -68,7 +68,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Cancelar</span></button>
                 <h4 class="modal-title" id="agregarDocumentoLabel">Agregar Documento</h4>
             </div>
 
